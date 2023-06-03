db5558a10b61a17ff6c0558e94a27e647ca07756
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-03 19:36:17 +0000

    Alpha 0.2.0-alpha31 Build

---
+++ 0.2.0-alpha31/framer/components/Thumbnail/Thumbnail.stories.js
@@ -0,0 +1,1 @@
+import*as t from"react";import{Thumbnail as o}from"./Thumbnail.js";var r={title:"rookies/Thumbnail",component:o},m={args:{src:"https://github.com/42-world/42world-Frontend/raw/dev/public/assets/42worldCharacterLogo.png",alt:"42world",width:269,ratio:"16:9"}},n=r;export{m as Primary,n as default};
---
+++ 0.2.0-alpha31/types/components/Sample/index.d.ts
@@ -0,0 +1,2 @@
+export * from './Sample';
+//# sourceMappingURL=index.d.ts.map
