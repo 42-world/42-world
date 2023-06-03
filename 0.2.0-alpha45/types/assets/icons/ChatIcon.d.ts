0e29ed513ec7d8df1db24c62342928c04930018b
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-03 20:40:47 +0000

    Remove Alpha 0.2.0-alpha

--- 0.2.0-alpha45/types/assets/icons/ChatIcon.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Color } from '../../common/token/color';
-interface Props {
-    color: Color;
-}
-export declare const ChatIcon: ({ color: colorName }: Props) => JSX.Element;
-export {};
-//# sourceMappingURL=ChatIcon.d.ts.map
