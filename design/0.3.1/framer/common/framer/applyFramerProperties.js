30f406b38734e1cd29271f627f88cd7a293203b2
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-04 15:34:38 +0000

    Version 0.3.1 Build (042dfdc05fa5efb1c34bb9391a24aa3955dc1f86)

---
+++ design/0.3.1/framer/common/framer/applyFramerProperties.js
@@ -0,0 +1,1 @@
+import*as e from"react";import{addPropertyControls as t}from"framer";function i(o,r){t(o,r)}export{i as applyFramerProperties};
---
+++ design/0.3.1/storybook/components-Input-Input-stories.d7dda345.iframe.bundle.js
@@ -0,0 +1,8 @@
+"use strict";(()=>{var E=Object.defineProperty,h=Object.defineProperties;var I=Object.getOwnPropertyDescriptors;var p=Object.getOwnPropertySymbols;var g=Object.prototype.hasOwnProperty,P=Object.prototype.propertyIsEnumerable;var u=(n,r,e)=>r in n?E(n,r,{enumerable:!0,configurable:!0,writable:!0,value:e}):n[r]=e,t=(n,r)=>{for(var e in r||(r={}))g.call(r,e)&&u(n,e,r[e]);if(p)for(var e of p(r))P.call(r,e)&&u(n,e,r[e]);return n},o=(n,r)=>h(n,I(r));(self.webpackChunk_42world_design_core=self.webpackChunk_42world_design_core||[]).push([[195],{"./src/components/Input/Input.stories.ts":(n,r,e)=>{var a,c,d;e.r(r),e.d(r,{Primary:()=>s,__namedExportsOrder:()=>_,default:()=>i});var l=e("./src/components/Input/Input.tsx");const m={title:"rookies/Input",component:l.I},s={args:{placeholder:"",maxLength:10,variant:"outline",hasError:!1}},i=m;s.parameters=o(t({},s.parameters),{docs:o(t({},(a=s.parameters)==null?void 0:a.docs),{source:t({originalSource:`{
+  args: {
+    placeholder: '',
+    maxLength: 10,
+    variant: 'outline',
+    hasError: false
+  }
+}`},(d=(c=s.parameters)==null?void 0:c.docs)==null?void 0:d.source)})});const _=["Primary"]}}]);})();
