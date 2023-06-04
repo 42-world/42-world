94dbf611b2026279bf28941eebada92767e75296
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-04 04:11:11 +0000

    Version 0.2.5 Build (2ffd7537fc7c86702b581ec38dbc49012ff159fd)

---
+++ design/latest/types/assets/icons/ThumbIcon.d.ts
@@ -0,0 +1,7 @@
+import type { Color } from '../../common/token/color';
+interface Props {
+    color: Color;
+}
+export declare const ThumbIcon: ({ color: colorName }: Props) => JSX.Element;
+export {};
+//# sourceMappingURL=ThumbIcon.d.ts.map
