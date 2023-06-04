035e5075276e74844e62a53dcc9a5af282705345
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-04 03:44:43 +0000

    Remove Alpha 0.2.3-alpha (01f2c2a2ebe94baf9d34250af8833c76501a8128))

--- 0.2.3-alpha50/types/assets/icons/BellIcon.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Color } from '../../common/token/color';
-interface Props {
-    color: Color;
-}
-export declare const BellIcon: ({ color: colorName }: Props) => JSX.Element;
-export {};
-//# sourceMappingURL=BellIcon.d.ts.map
--- 0.2.3-alpha50/types/assets/icons/CheckMarkIcon.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Color } from '../../common/token/color';
-interface Props {
-    color: Color;
-    className?: string;
-}
-export declare const CheckMarkIcon: ({ color: colorName, className }: Props) => JSX.Element;
-export {};
-//# sourceMappingURL=CheckMarkIcon.d.ts.map
