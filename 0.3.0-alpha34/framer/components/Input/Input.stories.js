59c5859e8f61f954f26415d09a12dbf49531815f
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-04 01:09:33 +0000

    Alpha 0.3.0-alpha34 Build (8ba1e25)

---
+++ 0.3.0-alpha34/framer/components/Input/Input.stories.js
@@ -0,0 +1,1 @@
+import*as t from"react";import{Input as o}from"./Input.js";var e={title:"rookies/Input",component:o},n={args:{value:"",placeholder:"",maxLength:10}},a=e;export{n as Primary,a as default};
---
+++ 0.3.0-alpha34/types/components/Input/Input.d.ts
@@ -0,0 +1,13 @@
+import type { InputHTMLAttributes, ReactNode } from 'react';
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+    label?: string;
+    value?: string;
+    defaultValue?: string;
+    subLabel?: string;
+    variant: 'outline' | 'filled';
+    hasError: boolean;
+    rightAddon: ReactNode;
+}
+export declare const Input: import("react").ForwardRefExoticComponent<Props & import("react").RefAttributes<HTMLInputElement>>;
+export {};
+//# sourceMappingURL=Input.d.ts.map
