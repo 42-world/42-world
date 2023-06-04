4be62067feba765c0d3f09af5f0e7e6fe7dac7d2
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-04 04:06:56 +0000

    Alpha 0.2.5-alpha52 Build (af4cc3d)

---
+++ design/0.2.5-alpha52/storybook/components-GroupHeader-GroupHeader-stories.21b077fb.iframe.bundle.js
@@ -0,0 +1,6 @@
+"use strict";(()=>{var g=Object.defineProperty,i=Object.defineProperties;var C=Object.getOwnPropertyDescriptors;var u=Object.getOwnPropertySymbols;var w=Object.prototype.hasOwnProperty,h=Object.prototype.propertyIsEnumerable;var p=(o,r,e)=>r in o?g(o,r,{enumerable:!0,configurable:!0,writable:!0,value:e}):o[r]=e,t=(o,r)=>{for(var e in r||(r={}))w.call(r,e)&&p(o,e,r[e]);if(u)for(var e of u(r))h.call(r,e)&&p(o,e,r[e]);return o},a=(o,r)=>i(o,C(r));(self.webpackChunk_42world_design_core=self.webpackChunk_42world_design_core||[]).push([[721],{"./src/components/GroupHeader/GroupHeader.stories.ts":(o,r,e)=>{var n,d,c;e.r(r),e.d(r,{Primary:()=>s,__namedExportsOrder:()=>E,default:()=>l});var m=e("./src/components/GroupHeader/GroupHeader.tsx");const _={title:"rookies/GroupHeader",component:m.t},s={args:{text:"\uC778\uAE30\uAC8C\uC2DC\uAE00",href:"https://www.google.com"}},l=_;s.parameters=a(t({},s.parameters),{docs:a(t({},(n=s.parameters)==null?void 0:n.docs),{source:t({originalSource:`{
+  args: {
+    text: '\uC778\uAE30\uAC8C\uC2DC\uAE00',
+    href: 'https://www.google.com'
+  }
+}`},(c=(d=s.parameters)==null?void 0:d.docs)==null?void 0:c.source)})});const E=["Primary"]}}]);})();
