4ce55273361bdab8b3fd99273f50528dc015bf7b
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-04 04:01:09 +0000

    Remove Alpha 0.2.4-alpha (996a16d7c361bd46ac82ef7dab5e85d19c81653e))

--- design/0.2.4-alpha51/types/assets/icons/SearchIcon.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Color } from '../../common/token/color';
-interface Props {
-    color: Color;
-}
-export declare const SearchIcon: ({ color: colorName }: Props) => JSX.Element;
-export {};
-//# sourceMappingURL=SearchIcon.d.ts.map
--- design/0.2.4-alpha51/types/components/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { Card } from './Card';
-export { Chip } from './Chip';
-export { GroupHeader } from './GroupHeader';
-export { Sample } from './Sample';
-export { Text } from './Text';
-export { Thumbnail } from './Thumbnail';
-//# sourceMappingURL=index.d.ts.map
