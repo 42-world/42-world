5ad5a23108d64ed2210fd2cf01d7e233579f8cf8
Seojunhwan <Seojunhwan@users.noreply.github.com>
2023-06-04 08:58:43 +0000

    Alpha 0.3.0-alpha34 Build (39eba5c)

--- design/0.3.0-alpha34/types/components/index.d.ts
+++ design/0.3.0-alpha34/types/components/index.d.ts
@@ -4,4 +4,5 @@ export { GroupHeader } from './GroupHeader';
 export { Sample } from './Sample';
 export { Text } from './Text';
 export { Thumbnail } from './Thumbnail';
+export { Input } from './Input';
 //# sourceMappingURL=index.d.ts.map
