c9bc5648651bb87008580eba003758447c9eaffa
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-04 03:41:20 +0000

    Alpha 0.2.3-alpha50 Build (00b1236)

---
+++ design/0.2.3-alpha50/storybook/components-Card-Card-stories.bd5d9542.iframe.bundle.js
@@ -0,0 +1,23 @@
+"use strict";(()=>{var _=Object.defineProperty,S=Object.defineProperties;var w=Object.getOwnPropertyDescriptors;var g=Object.getOwnPropertySymbols;var T=Object.prototype.hasOwnProperty,x=Object.prototype.propertyIsEnumerable;var y=(C,r,u)=>r in C?_(C,r,{enumerable:!0,configurable:!0,writable:!0,value:u}):C[r]=u,a=(C,r)=>{for(var u in r||(r={}))T.call(r,u)&&y(C,u,r[u]);if(g)for(var u of g(r))x.call(r,u)&&y(C,u,r[u]);return C},s=(C,r)=>S(C,w(r));(self.webpackChunk_42world_design_core=self.webpackChunk_42world_design_core||[]).push([[518],{"./src/components/Card/Card.stories.ts":(C,r,u)=>{var i,m,c,n,d,p,A,l,B;u.r(r),u.d(r,{Primary:()=>e,PrimaryNoPrimaryText:()=>t,PrimaryWithDetail:()=>o,__namedExportsOrder:()=>P,default:()=>E});var D=u("./src/components/Card/Card.tsx");const h={title:"rookies/Card",component:D.Z},e={args:{imageSrc:"https://picsum.photos/536/354",imageAlt:"42world",primary:"\uC5D0\uBCA0\uBCA0\uC190",secondary:"2023.04.32"}},o={args:{imageSrc:"https://picsum.photos/536/354",imageAlt:"42world",primary:"\uC5D0\uBCA0\uBCA0\uC190",secondary:"2023.04.32",detail:"\uC9C0\uCD5C\uB294 \uBC30\uAC00 \uACE0\uD504\uACE0 \uB0B4\uC77C\uC740 \uB2ED\uAC00\uC2B4\uC0B4 \uC18C\uC138\uC9C0 \uBA39\uC744 \uAC70\uACE0 \uC2A4\uD0C0\uBC85\uC2A4 \uAC00\uC11C \uC774\uAC70 \uD558\uACE0"}},t={args:{imageSrc:"https://picsum.photos/536/354",imageAlt:"42world",secondary:"\uC9C0\uCD5C",detail:"\uC9C0\uCD5C\uB294 \uBC30\uAC00 \uACE0\uD504\uACE0 \uB0B4\uC77C\uC740 \uB2ED\uAC00\uC2B4\uC0B4 \uC18C\uC138\uC9C0 \uBA39\uC744 \uAC70\uACE0 \uC2A4\uD0C0\uBC85\uC2A4 \uAC00\uC11C \uC774\uAC70 \uD558\uACE0"}},E=h;e.parameters=s(a({},e.parameters),{docs:s(a({},(i=e.parameters)==null?void 0:i.docs),{source:a({originalSource:`{
+  args: {
+    imageSrc: 'https://picsum.photos/536/354',
+    imageAlt: '42world',
+    primary: '\uC5D0\uBCA0\uBCA0\uC190',
+    secondary: '2023.04.32'
+  }
+}`},(c=(m=e.parameters)==null?void 0:m.docs)==null?void 0:c.source)})}),o.parameters=s(a({},o.parameters),{docs:s(a({},(n=o.parameters)==null?void 0:n.docs),{source:a({originalSource:`{
+  args: {
+    imageSrc: 'https://picsum.photos/536/354',
+    imageAlt: '42world',
+    primary: '\uC5D0\uBCA0\uBCA0\uC190',
+    secondary: '2023.04.32',
+    detail: '\uC9C0\uCD5C\uB294 \uBC30\uAC00 \uACE0\uD504\uACE0 \uB0B4\uC77C\uC740 \uB2ED\uAC00\uC2B4\uC0B4 \uC18C\uC138\uC9C0 \uBA39\uC744 \uAC70\uACE0 \uC2A4\uD0C0\uBC85\uC2A4 \uAC00\uC11C \uC774\uAC70 \uD558\uACE0'
+  }
+}`},(p=(d=o.parameters)==null?void 0:d.docs)==null?void 0:p.source)})}),t.parameters=s(a({},t.parameters),{docs:s(a({},(A=t.parameters)==null?void 0:A.docs),{source:a({originalSource:`{
+  args: {
+    imageSrc: 'https://picsum.photos/536/354',
+    imageAlt: '42world',
+    secondary: '\uC9C0\uCD5C',
+    detail: '\uC9C0\uCD5C\uB294 \uBC30\uAC00 \uACE0\uD504\uACE0 \uB0B4\uC77C\uC740 \uB2ED\uAC00\uC2B4\uC0B4 \uC18C\uC138\uC9C0 \uBA39\uC744 \uAC70\uACE0 \uC2A4\uD0C0\uBC85\uC2A4 \uAC00\uC11C \uC774\uAC70 \uD558\uACE0'
+  }
+}`},(B=(l=t.parameters)==null?void 0:l.docs)==null?void 0:B.source)})});const P=["Primary","PrimaryWithDetail","PrimaryNoPrimaryText"]}}]);})();
