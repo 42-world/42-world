4918dac6f1b4227547f781b55310fc67fe02eae9
Seojunhwan <Seojunhwan@users.noreply.github.com>
2023-06-05 01:04:33 +0000

    Remove Alpha 0.3.3-alpha (e31af323f14cffbcc7da4d2e5db9592e7fb1237c))

--- design/0.3.3-alpha56/types/components/Chip/Chip.d.ts
+++
@@ -1,8 +0,0 @@
-type Props = {
-    children?: JSX.Element;
-    text?: string;
-    selected?: boolean;
-};
-export declare function Chip({ children, text, selected }: Props): import("react/jsx-runtime").JSX.Element;
-export {};
-//# sourceMappingURL=Chip.d.ts.map
