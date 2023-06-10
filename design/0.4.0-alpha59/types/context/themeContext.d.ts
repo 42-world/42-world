53b7b530ad53f95cf191096cff160705d4212741
chichoon <chichoon@users.noreply.github.com>
2023-06-10 15:38:28 +0000

    Remove Alpha 0.4.0-alpha (b291ea8479c3d71190e24c7dadf8a0d7948c28ec))

--- design/0.4.0-alpha59/types/context/themeContext.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-export type Theme = 'light' | 'dark';
-export declare const useTheme: (consumer?: string | undefined) => NonNullable<Theme>, ThemeProvider: import("react").Provider<Theme>;
-//# sourceMappingURL=themeContext.d.ts.map
