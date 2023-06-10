5adfa324873952e7eafdb38cab34424adebaada9
chichoon <chichoon@users.noreply.github.com>
2023-06-10 15:01:10 +0000

    Alpha 0.4.0-alpha59 Build (afc150f)

---
+++ design/0.4.0-alpha59/types/components/Card/Card.d.ts
@@ -0,0 +1,10 @@
+type Props = {
+    imageSrc: string;
+    imageAlt?: string;
+    primary?: string;
+    secondary?: string;
+    detail: string;
+};
+export declare function Card({ imageSrc, imageAlt, primary, secondary, detail }: Props): import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=Card.d.ts.map
