f02aa04bf5ed421ada3bfac621e078d1aa4edf2f
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-10 17:50:26 +0000

    Version 0.6.1 Build (226679a33394f6de33ccfc1f57698dfa6449408d)

---
+++ design/0.6.1/types/assets/icons/index.d.ts
@@ -0,0 +1,10 @@
+export * from './BellIcon';
+export * from './ChatIcon';
+export * from './CheckMarkIcon';
+export * from './ChevronRight';
+export * from './MoonIcon';
+export * from './PlusIcon';
+export * from './SearchIcon';
+export * from './SunIcon';
+export * from './ThumbIcon';
+//# sourceMappingURL=index.d.ts.map
