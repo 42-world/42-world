55b95d6cf2d930784bd15addd3460e9034d145ae
chichoon <chichoon@users.noreply.github.com>
2023-06-10 15:40:15 +0000

    Version 0.4.0 Build (b291ea8479c3d71190e24c7dadf8a0d7948c28ec)

---
+++ design/0.4.0/framer/assets/icons/MoonIcon.js
@@ -0,0 +1,1 @@
+import*as o from"react";import{token as t}from"../../common/token/index.js";var n=({color:r})=>o.createElement("svg",{viewBox:"0 0 20 20",fill:"currentColor",width:"24",height:"24",color:t.color[r]},o.createElement("path",{d:"M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"}));export{n as MoonIcon};
