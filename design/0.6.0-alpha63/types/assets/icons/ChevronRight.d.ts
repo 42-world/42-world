a26d9b0caebff6483a5a0da81c14d7d4f13027a7
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-10 17:01:25 +0000

    Alpha 0.6.0-alpha63 Build (51c3c49)

---
+++ design/0.6.0-alpha63/types/assets/icons/ChevronRight.d.ts
@@ -0,0 +1,5 @@
+import { SVGAttributes } from 'react';
+type Props = SVGAttributes<SVGElement>;
+export declare const ChevronRight: (props: Props) => import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=ChevronRight.d.ts.map
