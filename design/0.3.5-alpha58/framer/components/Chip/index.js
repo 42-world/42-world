d96321daefcdd2d6dfd59e8dc9b2a4f26ece1911
hyongti <hyongti@users.noreply.github.com>
2023-06-10 15:20:16 +0000

    Remove Alpha 0.3.5-alpha (0b1e0c07e5463ffb2b2585981926eec089d33f52))

--- design/0.3.5-alpha58/framer/components/Chip/index.js
+++
@@ -1,1 +0,0 @@
-import*as r from"react";import{Chip as e}from"./Chip.js";export{e as Chip};
--- design/0.3.5-alpha58/types/components/index.d.ts
+++
@@ -1,9 +0,0 @@
-export { Card } from './Card';
-export { Chip } from './Chip';
-export { GroupHeader } from './GroupHeader';
-export { Sample } from './Sample';
-export { Text } from './Text';
-export { Thumbnail } from './Thumbnail';
-export { Divider } from './Divider';
-export { Input } from './Input';
-//# sourceMappingURL=index.d.ts.map
