4cbd581ad5cdf7534bee9585ca71a11bfc44145e
chichoon <chichoon@users.noreply.github.com>
2023-06-11 04:34:55 +0000

    Alpha 0.9.0-alpha64 Build (49d00ad)

---
+++ design/0.9.0-alpha64/storybook/components-Badge-Badge-stories.c08de742.iframe.bundle.js
@@ -0,0 +1,7 @@
+"use strict";(()=>{var i=Object.defineProperty,p=Object.defineProperties;var E=Object.getOwnPropertyDescriptors;var m=Object.getOwnPropertySymbols;var P=Object.prototype.hasOwnProperty,y=Object.prototype.propertyIsEnumerable;var u=(o,r,e)=>r in o?i(o,r,{enumerable:!0,configurable:!0,writable:!0,value:e}):o[r]=e,a=(o,r)=>{for(var e in r||(r={}))P.call(r,e)&&u(o,e,r[e]);if(m)for(var e of m(r))y.call(r,e)&&u(o,e,r[e]);return o},n=(o,r)=>p(o,E(r));(self.webpackChunk_42world_design_core=self.webpackChunk_42world_design_core||[]).push([[65],{"./src/components/Badge/Badge.stories.ts":(o,r,e)=>{var t,d,c;e.r(r),e.d(r,{Primary:()=>s,__namedExportsOrder:()=>l,default:()=>B});var _=e("./src/components/Badge/Badge.tsx");const g={title:"rookies/Badge",component:_.C},s={args:{color:"mono",text:"\uB0B4\uB313\uAE00"}},B=g;s.parameters=n(a({},s.parameters),{docs:n(a({},(t=s.parameters)==null?void 0:t.docs),{source:a({originalSource:`{
+  // Add your story args here
+  args: {
+    color: 'mono',
+    text: '\uB0B4\uB313\uAE00'
+  }
+}`},(c=(d=s.parameters)==null?void 0:d.docs)==null?void 0:c.source)})});const l=["Primary"]}}]);})();
---
+++ design/0.9.0-alpha64/types/components/IconText/IconText.d.ts
@@ -0,0 +1,9 @@
+import { FunctionComponent, SVGProps } from 'react';
+interface Props {
+    svg: FunctionComponent<SVGProps<SVGSVGElement>>;
+    text: string;
+    size?: 'medium' | 'small' | 'xsmall';
+}
+export declare function IconText({ svg, size, text }: Props): import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=IconText.d.ts.map
