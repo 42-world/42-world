bd14537d24f2a303fc6570b961c9580bef99f06d
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-11 03:31:35 +0000

    Alpha 0.8.0-alpha68 Build (c6a1772)

---
+++ design/0.8.0-alpha68/framer/components/Icon/Icon.js
@@ -0,0 +1,1 @@
+import*as r from"react";import{createElement as o}from"react";function a({svg:e,...t}){return o(e,t)}export{a as Icon};
---
+++ design/0.8.0-alpha68/types/components/Icon/Icon.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+interface Props extends React.SVGProps<SVGSVGElement> {
+    svg: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+}
+export declare function Icon({ svg, ...rest }: Props): import("react").FunctionComponentElement<import("react").SVGProps<SVGSVGElement>>;
+export {};
+//# sourceMappingURL=Icon.d.ts.map
