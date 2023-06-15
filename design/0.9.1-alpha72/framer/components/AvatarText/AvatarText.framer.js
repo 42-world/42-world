324ed887cd7c13ff7f3774aac5386da1630305ce
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-15 17:52:04 +0000

    Remove Alpha 0.9.1-alpha (811c32a767a79b3abe788dbb6bdcd694b4affea0))

--- design/0.9.1-alpha72/framer/components/AvatarText/AvatarText.framer.js
+++
@@ -1,1 +0,0 @@
-import*as r from"react";import{FramerProvider as o}from"../../common/framer/index.js";import{AvatarText as e}from"./AvatarText.js";function n(t){return r.createElement(o,null,r.createElement(e,{...t}))}export{n as AvatarText};
