0223ec11fac602f7af75556c48ecf6df0d174494
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-15 14:44:03 +0000

    Alpha 0.8.1-alpha69 Build (5682792)

--- design/0.8.1-alpha69/types/components/Icon/Icon.d.ts
+++ design/0.8.1-alpha69/types/components/Icon/Icon.d.ts
@@ -3,6 +3,6 @@ interface Props extends SVGProps<SVGSVGElement> {
     svg: React.FC<React.SVGAttributes<SVGElement>>;
     size?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge';
 }
-export declare function Icon({ svg: SvgComponent, size, className, ...rest }: Props): import("react").ReactSVGElement;
+export declare function Icon({ svg: SvgComponent, size, className, ...rest }: Props): import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=Icon.d.ts.map
