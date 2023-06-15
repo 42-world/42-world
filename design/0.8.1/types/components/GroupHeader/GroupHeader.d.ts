4dcfb0f1209e41b0fab53eec9e7f866324ff3a04
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-15 16:53:19 +0000

    Version 0.8.1 Build (ccd971118977c0470fb2be01e4218d07dfe129fe)

---
+++ design/0.8.1/types/components/GroupHeader/GroupHeader.d.ts
@@ -0,0 +1,7 @@
+interface Props {
+    href: string;
+    text: string;
+}
+export declare function GroupHeader({ href, text }: Props): import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=GroupHeader.d.ts.map
--- design/latest/types/assets/icons/ChatBubble.d.ts
+++
@@ -1,5 +0,0 @@
-import { SVGAttributes } from 'react';
-type Props = SVGAttributes<SVGElement>;
-export declare const ChatBubble: (props: Props) => import("react/jsx-runtime").JSX.Element;
-export {};
-//# sourceMappingURL=ChatBubble.d.ts.map
