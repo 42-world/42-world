25c412d4ad6ec9440783df75e36c6d83aa5636c9
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-16 09:13:53 +0000

    Version 0.11.1 Build (3f6d78c47a422890dc05522fa89b44f79accc663)

---
+++ design/0.11.1/types/components/Chip/Chip.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+type Props = {
+    icon?: React.FC<React.SVGAttributes<SVGElement>>;
+    text?: string;
+    selected?: boolean;
+};
+export declare function Chip({ icon, text, selected }: Props): import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=Chip.d.ts.map
