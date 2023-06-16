48a57294eaf534a34fdd01e5e6151b2e62b0e979
chichoon <chichoon@users.noreply.github.com>
2023-06-16 07:59:35 +0000

    Alpha 0.11.0-alpha64 Build (08329b5)

---
+++ design/0.11.0-alpha64/storybook/components-Comment-Comment-stories.140c1373.iframe.bundle.js
@@ -0,0 +1,14 @@
+"use strict";(()=>{var l=Object.defineProperty,E=Object.defineProperties;var D=Object.getOwnPropertyDescriptors;var m=Object.getOwnPropertySymbols;var p=Object.prototype.hasOwnProperty,_=Object.prototype.propertyIsEnumerable;var A=(s,u,C)=>u in s?l(s,u,{enumerable:!0,configurable:!0,writable:!0,value:C}):s[u]=C,t=(s,u)=>{for(var C in u||(u={}))p.call(u,C)&&A(s,C,u[C]);if(m)for(var C of m(u))_.call(u,C)&&A(s,C,u[C]);return s},n=(s,u)=>E(s,D(u));(self.webpackChunk_42world_design_core=self.webpackChunk_42world_design_core||[]).push([[196],{"./src/components/Comment/Comment.stories.ts":(s,u,C)=>{var r,o,a;C.r(u),C.d(u,{Primary:()=>e,__namedExportsOrder:()=>d,default:()=>c});var B=C("./src/components/Comment/Comment.tsx");const i={title:"rookies/Comment",component:B.s},e={args:{avatarSrc:"https://picsum.photos/536/354",avatarAlt:"WeL0ve42World",username:"\uC5D0\uBCA0\uBCA0\uC190",time:"3\uC2DC\uAC04 \uC804",content:"\uC624\uB298 \uC800\uB141\uC740 \uB9DB\uC788\uB294 \uB5A1\uAC08\uBE44\uC5D0 \uC5F4\uBB34\uAE40\uCE58\uC5D0 \uBA85\uC774\uB098\uBB3C\uC744 \uBA39\uC5C8\uACE0 \uB3D9\uBB3C\uC758\uC232 \uD558\uACE0\uC2F6\uACE0 \uB3C8\uBC8C\uACE0\uC2F6\uACE0 \uC5B4\uCA4C\uAD6C\uC800\uCA4C\uAD6C \uC544\uC27D\uAC8C\uB3C4 transition\uC740 \uC544\uC9C1 \uB9AC\uC561\uD2B8 18\uC5D0\uC11C\uB3C4 \uAC1C\uBC1C\uC911\uC778 \uAE30\uB2A5 \uC911 \uD558\uB098\uC774\uAE30\uC5D0...",isMine:!1,isAuthor:!1,isLiked:!1,numOfLikes:0}},c=i;e.parameters=n(t({},e.parameters),{docs:n(t({},(r=e.parameters)==null?void 0:r.docs),{source:t({originalSource:`{
+  // Add your story args here
+  args: {
+    avatarSrc: 'https://picsum.photos/536/354',
+    avatarAlt: 'WeL0ve42World',
+    username: '\uC5D0\uBCA0\uBCA0\uC190',
+    time: '3\uC2DC\uAC04 \uC804',
+    content: '\uC624\uB298 \uC800\uB141\uC740 \uB9DB\uC788\uB294 \uB5A1\uAC08\uBE44\uC5D0 \uC5F4\uBB34\uAE40\uCE58\uC5D0 \uBA85\uC774\uB098\uBB3C\uC744 \uBA39\uC5C8\uACE0 \uB3D9\uBB3C\uC758\uC232 \uD558\uACE0\uC2F6\uACE0 \uB3C8\uBC8C\uACE0\uC2F6\uACE0 \uC5B4\uCA4C\uAD6C\uC800\uCA4C\uAD6C \uC544\uC27D\uAC8C\uB3C4 transition\uC740 \uC544\uC9C1 \uB9AC\uC561\uD2B8 18\uC5D0\uC11C\uB3C4 \uAC1C\uBC1C\uC911\uC778 \uAE30\uB2A5 \uC911 \uD558\uB098\uC774\uAE30\uC5D0...',
+    isMine: false,
+    isAuthor: false,
+    isLiked: false,
+    numOfLikes: 0
+  }
+}`},(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source)})});const d=["Primary"]}}]);})();
