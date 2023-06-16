946fd0a74e65822a6bba44d44e6ffd391b66b635
chichoon <chichoon@users.noreply.github.com>
2023-06-16 08:47:37 +0000

    Alpha 0.12.0-alpha74 Build (5b9d5b7)

---
+++ design/0.12.0-alpha74/storybook/components-AvatarText-AvatarText-stories.f0ed48b0.iframe.bundle.js
@@ -0,0 +1,9 @@
+"use strict";(()=>{var x=Object.defineProperty,A=Object.defineProperties;var T=Object.getOwnPropertyDescriptors;var u=Object.getOwnPropertySymbols;var C=Object.prototype.hasOwnProperty,h=Object.prototype.propertyIsEnumerable;var m=(t,r,s)=>r in t?x(t,r,{enumerable:!0,configurable:!0,writable:!0,value:s}):t[r]=s,a=(t,r)=>{for(var s in r||(r={}))C.call(r,s)&&m(t,s,r[s]);if(u)for(var s of u(r))h.call(r,s)&&m(t,s,r[s]);return t},o=(t,r)=>A(t,T(r));(self.webpackChunk_42world_design_core=self.webpackChunk_42world_design_core||[]).push([[511],{"./src/components/AvatarText/AvatarText.stories.ts":(t,r,s)=>{var n,c,d;s.r(r),s.d(r,{Primary:()=>e,__namedExportsOrder:()=>l,default:()=>_});var p=s("./src/components/AvatarText/AvatarText.tsx");const i={title:"rookies/AvatarText",component:p.M},e={args:{src:"https://picsum.photos/536/354",alt:"42world",size:"40px",mainText:"\uADDC\uB3D9\uD55C\uADF8\uB987\uC8FC\uC18C"}},_=i;e.parameters=o(a({},e.parameters),{docs:o(a({},(n=e.parameters)==null?void 0:n.docs),{source:a({originalSource:`{
+  // Add your story args here
+  args: {
+    src: 'https://picsum.photos/536/354',
+    alt: '42world',
+    size: '40px',
+    mainText: '\uADDC\uB3D9\uD55C\uADF8\uB987\uC8FC\uC18C'
+  }
+}`},(d=(c=e.parameters)==null?void 0:c.docs)==null?void 0:d.source)})});const l=["Primary"]}}]);})();
