5cc4cd4c3200128530da764f255b59627d934f5c
asdf99245 <asdf99245@users.noreply.github.com>
2023-06-16 05:55:07 +0000

    Version 0.10.0 Build (bac5573bba722820875b118f3f1f7684f4bdcf26)

---
+++ design/0.10.0/types/components/Button/Button.d.ts
@@ -0,0 +1,11 @@
+import { ButtonHTMLAttributes, FC, SVGAttributes } from 'react';
+type ButtonVariant = 'primary' | 'secondary' | 'text';
+interface ButtonProps {
+    variant?: ButtonVariant;
+    text?: string;
+    icon?: FC<SVGAttributes<SVGSVGElement>>;
+}
+type Props = ButtonProps & Omit<ButtonHTMLAttributes<HTMLButtonElement>, keyof ButtonProps>;
+export declare function Button({ variant, text, icon, ...restProps }: Props): import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=Button.d.ts.map
