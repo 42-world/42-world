8aa87bf6a737f35d68fc364c44e76a98e9a94875
asdf99245 <asdf99245@users.noreply.github.com>
2023-06-17 16:42:54 +0000

    Alpha 0.12.0-alpha78 Build (ee5179b)

--- design/0.12.0-alpha78/types/components/ListItem/ListItem.d.ts
+++ design/0.12.0-alpha78/types/components/ListItem/ListItem.d.ts
@@ -1,14 +1,12 @@
-import { PropsWithChildren } from 'react';
-type Props = PropsWithChildren<{
+import { ReactNode } from 'react';
+interface Props {
     title: string;
     thumbnailSrc?: string;
     secondaryTextFirst?: string;
     secondaryTextSecond?: string;
     hasBorder?: boolean;
-}>;
-export declare function ListItem({ children, title, thumbnailSrc, secondaryTextFirst, secondaryTextSecond, hasBorder, }: Props): import("react/jsx-runtime").JSX.Element;
-export declare namespace ListItem {
-    var MetadataItems: ({ children }: Pick<Props, "children">) => import("react/jsx-runtime").JSX.Element;
+    rightAddon?: ReactNode;
 }
+export declare function ListItem({ title, thumbnailSrc, secondaryTextFirst, secondaryTextSecond, hasBorder, rightAddon, }: Props): import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=ListItem.d.ts.map
