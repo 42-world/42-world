3e04a65351df7879cddf6d61aba18429707b8468
asdf99245 <asdf99245@users.noreply.github.com>
2023-06-17 17:57:01 +0000

    Alpha 0.13.0-alpha78 Build (687644f)

--- design/0.13.0-alpha78/types/components/ListItem/ListItem.d.ts
+++ design/0.13.0-alpha78/types/components/ListItem/ListItem.d.ts
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, MouseEventHandler } from 'react';
 interface Props {
     title: string;
     thumbnailSrc?: string;
@@ -6,7 +6,8 @@ interface Props {
     secondaryTextSecond?: string;
     hasBorder?: boolean;
     rightAddon?: ReactNode;
+    onClick?: MouseEventHandler<HTMLDivElement>;
 }
-export declare function ListItem({ title, thumbnailSrc, secondaryTextFirst, secondaryTextSecond, hasBorder, rightAddon, }: Props): import("react/jsx-runtime").JSX.Element;
+export declare function ListItem({ title, thumbnailSrc, secondaryTextFirst, secondaryTextSecond, hasBorder, rightAddon, onClick, }: Props): import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=ListItem.d.ts.map
