82ce24d209f2b2708f314515bf8fd8048c8c2e31
asdf99245 <asdf99245@users.noreply.github.com>
2023-06-20 17:11:35 +0000

    Alpha 0.14.1-alpha83 Build (4be5ad5)

---
+++ design/0.14.1-alpha83/types/components/Chip/Chip.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+type Props = {
+    icon?: React.FC<React.SVGAttributes<SVGElement>>;
+    text?: string;
+    isSelected?: boolean;
+};
+export declare function Chip({ icon, text, isSelected }: Props): import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=Chip.d.ts.map
