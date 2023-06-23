2768a3c9ed58674d744c6cb7854aeed7387def11
chichoon <chichoon@users.noreply.github.com>
2023-06-23 11:54:01 +0000

    Alpha 0.15.0-alpha85 Build (4ee2e1f)

---
+++ design/0.15.0-alpha85/framer/components/Footer/Footer.stories.js
@@ -0,0 +1,1 @@
+import*as t from"react";import{Footer as o}from"./Footer.js";var e={title:"rookies/Footer",component:o},a={args:{githubLink:"https://github.com",mailLink:"mailto:asdf@naver.com",text:"@ 2022. Team ROOK!ES"}},p=e;export{a as Primary,p as default};
