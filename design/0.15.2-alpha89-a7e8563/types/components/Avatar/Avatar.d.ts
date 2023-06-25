e5c3ee1942df7000885157bd6c21d916dce2058e
42world-official <42world-official@users.noreply.github.com>
2023-06-25 19:07:00 +0000

    Alpha 0.15.2-alpha89-a7e8563 Build

---
+++ design/0.15.2-alpha89-a7e8563/types/components/Avatar/Avatar.d.ts
@@ -0,0 +1,8 @@
+interface Props {
+    src: string;
+    alt?: string;
+    size?: '40px' | '64px';
+}
+export declare function Avatar({ src, alt, size }: Props): import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=Avatar.d.ts.map
