01c23cd48662528829cf92021f0c7ddd57cf5799
asdf99245 <asdf99245@users.noreply.github.com>
2023-06-26 12:16:31 +0000

    Alpha 0.16.0-alpha86-fc09137 Build

---
+++ design/0.16.0-alpha86-fc09137/framer/components/Badge/index.js
@@ -0,0 +1,1 @@
+import*as e from"react";import{Badge as t}from"./Badge.js";export{t as Badge};
---
+++ design/0.16.0-alpha86-fc09137/framer/utils/isDarkTheme.js
@@ -0,0 +1,1 @@
+import*as e from"react";function o(){return typeof window=="undefined"?!1:window.document.documentElement.classList.contains("dark")}export{o as isDarkTheme};
