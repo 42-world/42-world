cebb6ea1460927bec35aa6cf7dfade4c428de4d0
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-27 14:02:35 +0000

    [skip actions] Remove Alpha82 (03f3d6480a0e956f6ed41d1c3181df35d1dd4fec))

--- design/0.14.2-alpha82/types/components/CommentInput/CommentInput.d.ts
+++
@@ -1,6 +0,0 @@
-import type { InputHTMLAttributes } from 'react';
-interface CommentInputProps extends InputHTMLAttributes<HTMLInputElement> {
-}
-export declare const CommentInput: import("react").ForwardRefExoticComponent<CommentInputProps & import("react").RefAttributes<HTMLInputElement>>;
-export {};
-//# sourceMappingURL=CommentInput.d.ts.map
