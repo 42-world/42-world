15248d300172321a6fd2679c876b167f6548735c
asdf99245 <asdf99245@users.noreply.github.com>
2023-06-28 07:35:08 +0000

    Alpha 0.18.0-alpha87-3353a45 Build

---
+++ design/0.18.0-alpha87-3353a45/framer/components/Sample/index.js
@@ -0,0 +1,1 @@
+import*as e from"react";import{Sample as o}from"./Sample.js";export{o as Sample};
---
+++ design/0.18.0-alpha87-3353a45/types/components/Card/Card.d.ts
@@ -0,0 +1,10 @@
+type Props = {
+    imageSrc: string;
+    imageAlt?: string;
+    primary?: string;
+    secondary?: string;
+    title: string;
+};
+export declare function Card({ imageSrc, imageAlt, primary, secondary, title }: Props): import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=Card.d.ts.map
