39fec840330fa45bb538bf36f1c60e433f79eddf
asdf99245 <asdf99245@users.noreply.github.com>
2023-06-29 16:43:29 +0000

    Alpha 0.20.0-alpha108-07da4c5 Build

---
+++ design/0.20.0-alpha108-07da4c5/framer/components/Radio/Radio.framer.js
@@ -0,0 +1,1 @@
+import*as r from"react";import{FramerProvider as m}from"../../common/framer/index.js";import{Radio as e}from"./Radio.js";function f(o){return r.createElement(m,null,r.createElement(e,{...o}))}export{f as Radio};
