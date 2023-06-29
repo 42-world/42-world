ca7856fa997fe73e8df61bda1274d4ef509747da
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-29 16:09:09 +0000

    Alpha 0.19.3-alpha107-2769e48 Build

---
+++ design/0.19.3-alpha107-2769e48/framer/components/Checkbox/Checkbox.stories.js
@@ -0,0 +1,1 @@
+import*as o from"react";import{Checkbox as e}from"./Checkbox.js";var t={title:"rookies/Checkbox",component:e},p={args:{labelText:"label",checked:!1,disabled:!1}},c=t;export{p as Primary,c as default};
---
+++ design/0.19.3-alpha107-2769e48/types/components/SearchBar/SearchBar.d.ts
@@ -0,0 +1,8 @@
+import { InputHTMLAttributes } from 'react';
+type SearchBarVariant = 'primary' | 'solid';
+interface InputProps {
+    variant: SearchBarVariant;
+}
+export declare const SearchBar: import("react").ForwardRefExoticComponent<InputProps & Omit<InputHTMLAttributes<HTMLInputElement>, "variant"> & import("react").RefAttributes<HTMLInputElement>>;
+export {};
+//# sourceMappingURL=SearchBar.d.ts.map
