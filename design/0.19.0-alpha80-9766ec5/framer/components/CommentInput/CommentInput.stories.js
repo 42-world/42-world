6123932bc99c05339815a109997f31c301738e8c
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-29 14:39:00 +0000

    [skip actions] Remove Alpha80 (50c7fa1d077938355d43605cc0fdd9b9a34850b1))

--- design/0.19.0-alpha80-9766ec5/framer/components/CommentInput/CommentInput.stories.js
+++
@@ -1,1 +0,0 @@
-import*as t from"react";import{CommentInput as o}from"./CommentInput.js";var e={title:"rookies/CommentInput",component:o},p={args:{placeholder:"Lorem ipsum"}},n=e;export{p as Primary,n as default};
