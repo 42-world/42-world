fc4b92946be3591af17fef241607bd471c0d8c3a
asdf99245 <asdf99245@users.noreply.github.com>
2023-06-29 12:32:15 +0000

    Alpha 0.18.1-alpha100-64f1357 Build

---
+++ design/0.18.1-alpha100-64f1357/types/components/Thumbnail/Thumbnail.d.ts
@@ -0,0 +1,11 @@
+type Props = {
+    src: string;
+    alt?: string;
+    width: number;
+    rounded?: '8px' | '16px';
+    ratio?: '16:9' | '16:10' | '1:1';
+    className?: string;
+};
+export declare function Thumbnail({ src, alt, width, rounded, ratio, className }: Props): import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=Thumbnail.d.ts.map
