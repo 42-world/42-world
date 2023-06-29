e30557d95d1a311ece3c40c2c03ef40c056ba846
Skyrich2000 <Skyrich2000@users.noreply.github.com>
2023-06-29 20:50:47 +0000

    Alpha 0.19.4-alpha109-396dc18 Build

---
+++ design/0.19.4-alpha109-396dc18/framer/common/framer/applyFramerProperties.js
@@ -0,0 +1,1 @@
+import*as e from"react";import{addPropertyControls as p}from"framer";function a(o,r){p(o,r)}export{a as applyFramerProperties};
---
+++ design/0.19.4-alpha109-396dc18/types/components/Chip/Chip.d.ts
@@ -0,0 +1,9 @@
+import { ReactElement } from 'react';
+type Props = {
+    text?: string;
+    isSelected?: boolean;
+    icon?: ReactElement;
+};
+export declare function Chip({ icon, text, isSelected }: Props): import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=Chip.d.ts.map
