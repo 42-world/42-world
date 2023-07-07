9fb1c5cedd2c81812d20a4ea46a3f61e2593d628
Seojunhwan <Seojunhwan@users.noreply.github.com>
2023-07-07 13:38:41 +0000

    Alpha 0.21.2-alpha110-a40222f Build

---
+++ design/0.21.2-alpha110-a40222f/types/components/Badge/Badge.d.ts
@@ -0,0 +1,8 @@
+interface Props {
+    color?: 'indigo' | 'yellow' | 'mint' | 'cyan' | 'blue' | 'pink' | 'brown' | 'outline' | 'mono';
+    text: string;
+    className?: string;
+}
+export declare function Badge({ color, text, className }: Props): import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=Badge.d.ts.map
---
+++ design/0.21.2-alpha110-a40222f/types/components/Divider/Divider.d.ts
@@ -0,0 +1,7 @@
+import type { HTMLAttributes } from 'react';
+interface DividerProps extends HTMLAttributes<HTMLDivElement> {
+    variant: 'primary' | 'secondary';
+}
+export declare function Divider({ variant, className, ...restProps }: DividerProps): import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=Divider.d.ts.map
