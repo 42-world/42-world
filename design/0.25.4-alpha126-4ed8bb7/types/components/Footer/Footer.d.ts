15317c9715ad848c2545aa047863b9ff65c6fe40
Seojunhwan <Seojunhwan@users.noreply.github.com>
2023-07-22 14:01:14 +0000

    Alpha 0.25.4-alpha126-4ed8bb7 Build

---
+++ design/0.25.4-alpha126-4ed8bb7/types/components/Footer/Footer.d.ts
@@ -0,0 +1,8 @@
+interface Props {
+    githubLink?: string;
+    mailLink?: string;
+    text: string;
+}
+export declare function Footer({ githubLink, mailLink, text }: Props): import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=Footer.d.ts.map
---
+++ design/0.25.4-alpha126-4ed8bb7/types/utils/index.d.ts
@@ -0,0 +1,8 @@
+export * from './isDarkTheme';
+export * from './isOsDarkTheme';
+export * from './setTheme';
+export * from './toggleTheme';
+export * from './composeEvent';
+export * from './createContext';
+export * from './cn';
+//# sourceMappingURL=index.d.ts.map
